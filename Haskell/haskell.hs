6a756529c9d6c00e6bcc48cf520e7cf924670dc1
ZackMagleby <zackmagleby@yahoo.com>
2018-04-11 08:53:12 -0600

    eh

---
+++ Haskell/haskell.hs
@@ -0,0 +1,16 @@
+import Data.Array
+
+-- Type Signature
+isPrime :: Int -> Bool
+
+-- Type Signature
+primes :: [Int]
+
+-- Type Signature
+isPrimeFast :: Int -> Bool
+
+-- Type Signature
+primesFast :: [Int]
+
+iSqrt :: Int-> Int
+iSqrt n = floor(sqrt(fromIntegral n))
